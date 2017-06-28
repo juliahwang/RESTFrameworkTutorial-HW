3996ffe030e6425ee29f081c446b8d8bd0cb34e8
JuliaHwang <qufskan9396@gmail.com>
2017-06-28 22:38:17 +0900

    add ModelSerializer, modified BooleanField default to False, migrated

--- django_app/snippets/models.py
+++ django_app/snippets/models.py
@@ -13,7 +13,7 @@ class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-    linenos = models.BooleanField()
+    linenos = models.BooleanField(default=False)
     language = models.CharField(
         choices=LANGUAGE_CHOICES,
         default='python',
---
+++ django_app/snippets/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+
+from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
+
+
+# class SnippetSerializer(serializers.Serializer):
+#     # 직렬화/반직렬화될 필드를 선언
+#     pk = serializers.IntegerField(read_only=True)
+#     title = serializers.CharField(
+#         required=False,
+#         allow_blank=True,
+#         max_length=100
+#     )
+#     # style={'base_template': 'textarea.html'} 는
+#     # 장고 폼의 widget=widgets.Textarea 로 쓴 것과 같다.
+#     code = serializers.CharField(style={'base_template': 'textarea.html'})
+#     linenos = serializers.BooleanField(required=False)
+#     language = serializers.ChoiceField(
+#         choices=LANGUAGE_CHOICES,
+#         default='python',
+#     )
+#     style = serializers.ChoiceField(
+#         choices=STYLE_CHOICES,
+#         default='friendly'
+#     )
+#
+#     def create(self, validated_data):
+#         """
+#         검증한 데이터로 새 'Snippet' 인스턴스를 생성하고 리턴
+#         :param validated_data:
+#         :return: 새 Snippet 인스턴스
+#         """
+#         return Snippet.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         """
+#         검증한 데이터로 기존 'Snippet' 인스턴스를 업데이트한 후 리턴
+#         :param instance: Snippet의 인스턴스
+#         :param validated_data: 검증한 데이터
+#         :return: 인스턴스 수정후 리턴
+#         """
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.code = validated_data.get('code', instance.code)
+#         instance.linenos = validated_data.get('linenos', instance.linenos)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.style = validated_data.get('style', instance.style)
+#         instance.save()
+#         return instance
+
+
+# 모델 시리얼라이저 정의하기
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = (
+            'id',
+            'title',
+            'code',
+            'linenos',
+            'language',
+            'style',
+        )
