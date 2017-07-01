9f8d87ac5f22d6419a6442aee83a53311fba8ac3
JuliaHwang <qufskan9396@gmail.com>
2017-07-02 06:31:48 +0900

    add login url to the whole urlpatterns list

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-
+from django.conf.urls import include
 from snippets import views
 
 urlpatterns = [
@@ -10,5 +10,8 @@ urlpatterns = [
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
-
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [url(r'^api-auth/', include('rest_framework.urls',
+                                           namespace='rest_framework')),
+]
