889d5d983c6e3e0163ce52b07311d91cf1e53e0a
JuliaHwang <qufskan9396@gmail.com>
2017-07-02 07:39:01 +0900

    added names to every url patterns

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -7,15 +7,30 @@ urlpatterns = [
     # SnippetHighlight 뷰와 연결
     url(r'^$', views.api_root),
     # 코드조각 리스트
-    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/$',
+        views.SnippetList.as_view(),
+        name='snippet-list',
+        ),
     # 코드조각 디테일 페이지
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$',
+        views.SnippetDetail.as_view(),
+        name='snippet-detail',
+        ),
     # 하이라이트된 코드조각을 볼 수 있는 페이지
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        views.SnippetHighlight.as_view(),
+        name='snippet-highlight',
+        ),
     # 유저 리스트
-    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/$',
+        views.UserList.as_view(),
+        name='user-list',
+        ),
     # 유저별 디테일 페이지
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user-detail',
+        ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
