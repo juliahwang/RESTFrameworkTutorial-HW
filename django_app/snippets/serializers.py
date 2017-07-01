4ccbd757eb1d807d878e8603a59aa7957fa4522b
JuliaHwang <qufskan9396@gmail.com>
2017-07-01 22:49:14 +0900

    add serializerm, views, urls for User objects

--- django_app/snippets/serializers.py
+++ django_app/snippets/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
@@ -60,3 +61,14 @@ class SnippetSerializer(serializers.ModelSerializer):
             'language',
             'style',
         )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Snippet.objects.all(),
+    )
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
