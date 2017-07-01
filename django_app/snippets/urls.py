fb2056e46c6f3af5c0f4fae002a25be52d09a28e
JuliaHwang <qufskan9396@gmail.com>
2017-07-02 08:46:01 +0900

    refactored the whole urlpatterns using Router class

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,41 +1,19 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import include
+from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
+# 라우터 생성
+router = DefaultRouter()
+# 라우터에 뷰셋 등록
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# API url을 라우터가 자동으로 인식한다.
+# 추가로 탐색가능한 API를 구현하기 위해 로그인에 사용할 url은 따로 직접 설정.
 urlpatterns = [
-    # SnippetHighlight 뷰와 연결
-    url(r'^$', views.api_root),
-    # 코드조각 리스트
-    url(r'^snippets/$',
-        views.SnippetList.as_view(),
-        name='snippet-list',
-        ),
-    # 코드조각 디테일 페이지
-    url(r'^snippets/(?P<pk>[0-9]+)/$',
-        views.SnippetDetail.as_view(),
-        name='snippet-detail',
-        ),
-    # 하이라이트된 코드조각을 볼 수 있는 페이지
-    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
-        views.SnippetHighlight.as_view(),
-        name='snippet-highlight',
-        ),
-    # 유저 리스트
-    url(r'^users/$',
-        views.UserList.as_view(),
-        name='user-list',
-        ),
-    # 유저별 디테일 페이지
-    url(r'^users/(?P<pk>[0-9]+)/$',
-        views.UserDetail.as_view(),
-        name='user-detail',
-        ),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-# 탐색 가능한 API를 위한 로그인/로그아웃 뷰
-urlpatterns += [url(r'^api-auth/', include('rest_framework.urls',
-                                           namespace='rest_framework')),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth', include(
+        'rest_framework.urls',
+        namespace='rest_framework'))
 ]
