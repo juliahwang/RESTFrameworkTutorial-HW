61412e860421117e8f961c9f17a170031be78b07
JuliaHwang <qufskan9396@gmail.com>
2017-07-02 08:18:29 +0900

    refactored every views using viewsets class

--- django_app/snippets/views.py
+++ django_app/snippets/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions, renderers
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, renderers, viewsets
+from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
@@ -10,37 +10,73 @@ from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
-class SnippetList(generics.ListCreateAPIView):
+# class SnippetList(generics.ListCreateAPIView):
+#     """
+#     코드 조각을 모두 보여주거나 새 코드조각을 생성
+#     """
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+#
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+#
+#
+# class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+#     """
+#     코드조각 조회, 업데이트, 삭제
+#     :param request: 요청
+#     :param pk: 코드조각의 고유 pk
+#     :return: 조각을 조회, 업데이트, 삭제할 수 있는 페이지로 렌더
+#     """
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+#                           IsOwnerOrReadOnly, )
+#
+#
+# class SnippetHighlight(generics.GenericAPIView):
+#     queryset = Snippet.objects.all()
+#     renderer_classes = (renderers.StaticHTMLRenderer, )
+#
+#     def get(self, request, *args, **kwargs):
+#         snippet = self.get_object()
+#         return Response(snippet.highlighted)
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
     """
-    코드 조각을 모두 보여주거나 새 코드조각을 생성
+    해당 뷰셋은 'list', 'create', 'retrieve', 'update', 'destroy' 기능을 자동으로 지원.
+    highlight은 사용자화한 클래스이므로 메서드로 따로 정의
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+# class UserList(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# class UserDetail(generics.RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+# 위에 따로 정의해준 UserList, UserDetail 클래스를 하나로!
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    코드조각 조회, 업데이트, 삭제
-    :param request: 요청
-    :param pk: 코드조각의 고유 pk
-    :return: 조각을 조회, 업데이트, 삭제할 수 있는 페이지로 렌더
+    해당 뷰셋은 list와 detail 기능을 자동으로 지원한다.
     """
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly, )
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -52,11 +88,3 @@ def api_root(request, format=None):
         'snippets': reverse('snippet-list', request=request, format=format),
     })
 
-
-class SnippetHighlight(generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    renderer_classes = (renderers.StaticHTMLRenderer, )
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
