a483db86f20bda817c659963b53de789c4c5d7f6
JuliaHwang <qufskan9396@gmail.com>
2017-07-01 22:33:56 +0900

    added user and code fields to Snippet model, recreated DB and migrated

--- django_app/snippets/models.py
+++ django_app/snippets/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
@@ -13,6 +15,8 @@ class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
+    owner = models.ForeignKey('auth.User', related_name='snippets')
+    highlighted = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(
         choices=LANGUAGE_CHOICES,
@@ -27,3 +31,19 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ('created', )
+
+    def save(self, *args, **kwargs):
+        """
+        'pygments' 라이브러리를 활용하여 하이라이트된 코드를 만든다.
+        """
+        lexer = get_lexer_by_name(self.language)
+        linenos = self.linenos and 'table' or False
+        options = self.title and {'title': self.title} or {}
+        formatter = HtmlFormatter(
+            style=self.style,
+            linenos=linenos,
+            full=True,
+            **options,
+        )
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
